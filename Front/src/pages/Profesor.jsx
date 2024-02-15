9b2d0c62bf9c401c8ee902124eceabe5a2228cc8
Daniel <danielcaz@gmail.com>
2024-02-15 07:41:05 -0300

    visual de login y las rutas hardcodeadas a componentes visuales de ejemplo para los distintos roles

---
+++ Front/src/pages/Profesor.jsx
@@ -0,0 +1,7 @@
+export const Profesor = () => {
+    return (
+      <div className="w-full h-full flex justify-center items-center">
+        <h1 className="bg-blue-500">Profesor</h1>
+      </div>
+    );
+  };
