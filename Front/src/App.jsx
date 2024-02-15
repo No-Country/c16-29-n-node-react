f22432043aa89f8d507a3d8f4a5b64657b2404f3
Daniel <danielcaz@gmail.com>
2024-02-15 07:41:05 -0300

    visual de login y las rutas hardcodeadas a componentes visuales de ejemplo para los distintos roles

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -1,18 +1,28 @@
-import SelectWithFilters from "./components/SelectWithFilters"
-import { colourOptions } from './utils/data'
-import Sidebar from "./components/Sidebar"
+import { useRoutes, BrowserRouter } from "react-router-dom";
+import { Login } from "./components/Login";
+import { Directivo } from "./pages/Directivo"; 
+import { Profesor } from "./pages/Profesor";
+import { Tutor } from "./pages/Tutor";
+import { Alumno } from "./pages/Alumno";
+
+
+const AppRoutes = () => {
+  let routes = useRoutes([
+    { path: "/", element: <Login/> },
+    { path: "/directivo", element: <Directivo /> },
+    { path: "/profesor", element: <Profesor/> },
+    { path: "/tutor", element: <Tutor /> },
+    { path: "/alumno", element: <Alumno /> },    
+  ]);
+  return routes;
+};
 
 function App() {
-
   return (
-    <>
-      {/* <Sidebar /> */}
-      <div className="w-72">
-      <SelectWithFilters data={colourOptions} />
-      </div>
-      
-    </>
-  )
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
