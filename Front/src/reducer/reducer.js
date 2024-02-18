d6ff77f0f7ee2c3ef62e857ecf86eee20ae95c58
andrescn2020 <andrescn_2010@hotmail.com>
2024-02-18 11:54:04 -0300

    redux-installation

---
+++ Front/src/reducer/reducer.js
@@ -0,0 +1,16 @@
+const initialState = {
+    
+};
+
+function rootReducer(state = initialState, { type, payload }) {
+    switch (type) {
+        case 1:
+            return {
+                
+            }
+        default:
+            return state;
+    }
+}
+
+export default rootReducer;
