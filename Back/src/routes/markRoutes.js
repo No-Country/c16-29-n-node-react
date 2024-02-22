9f4fb9a736df1aa727f36bbf6cf8a1797f5fd2fc
emanuel-gauna <lemagauna@gmail.com>
2024-02-21 21:51:32 -0300

    creando  rutas , controllador y servico de Marks

---
+++ Back/src/routes/markRoutes.js
@@ -0,0 +1,19 @@
+const router = require("express").Router();
+const { verifyToken } = require("../middlewares/auth");
+const { getMarkById, getMarks, createMark, updateMark , getMarkByStudent } = require("../controllers/mark.controller");
+
+router
+      .get("/", verifyToken  ,getMarks )
+      .get("/:id", verifyToken , getMarkById )
+      .get("/current", verifyToken , getMarkByStudent )
+      .post("/exams",  verifyToken ,createMark )
+      .put("/:id", verifyToken , updateMark )
+
+
+      module.exports = router;
+    /*   POST /api/exams [TEACHER]
+POST /api/exams/marks [TEACHER]
+PUT /api/marks/:id [TEACHER]
+GET /api/exams/current [TEACHER]
+GET /api/exams/:id/marks [TEACHER]
+GET /api/marks/current [TUTOR, STUDENT] */
