6b37803f591249eb54a56ce1ce730fecd7639a1c
emanuel-gauna <lemagauna@gmail.com>
2024-02-23 18:33:50 -0300

    instalando method-override y modificando el app.js

--- Back/src/app.js
+++ Back/src/app.js
@@ -3,6 +3,7 @@ import express from "express"
 import cors from 'cors'
 
 require("dotenv").config();
+const methodOverride = require('method-override');
 
 //import db from './database/db.js'
 
@@ -16,7 +17,9 @@ const  [ markRouter, subjectsRouter ] = require("./routes");
 const app = express()
 
 app.use(cors())
+app.use(express.urlencoded({ extended: false }));//captura eventos de formulario
 app.use(express.json())
+app.use(methodOverride('_method'));//habilitar metodos put y delete wn los form (?_method=PUT)(?_method=DELETE)
 app.use('/pers', persRouter)
 app.use('/login', persRouter)
 app.use(`/api/marks`, markRouter);
