82afb9f76e9aec090e694021069209047ebc6563
Isaac Flores <isaacfloresva@gmail.com>
2024-02-24 13:02:56 -0700

    Update files for modify on DB

--- Back/d_config.js
+++ Back/d_config.js
@@ -3,10 +3,10 @@ import {config} from 'dotenv'
 config()
 
 export const PORT =  process.env.PORT || 5002
-export const DB_USER =  process.env.DB_USER || 'sql10684703'
-export const DB_PASSWORD =  process.env.DB_PASSWORD ||'bQveMu2qvd'
-export const DB_HOST =  process.env.DB_HOST || 'sql10.freemysqlhosting.net'
-export const DB_DATABASE =  process.env.DB_DATABASE || 'sql10684703'
+export const DB_USER =  process.env.DB_USER || 'uexulbl2ovselszb'
+export const DB_PASSWORD =  process.env.DB_PASSWORD ||'jcgobzIW3KkKaaDxGYYO'
+export const DB_HOST =  process.env.DB_HOST || 'btucvoqrbbkszrtziiis-mysql.services.clever-cloud.com'
+export const DB_DATABASE =  process.env.DB_DATABASE || 'btucvoqrbbkszrtziiis'
 export const DB_PORT =  process.env.DB_PORT ||3306
 
 //Datos para el token
