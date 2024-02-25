5b593dd85512712388267b8f68b66032d6d9c699
Isaac Flores <isaacfloresva@gmail.com>
2024-02-24 17:39:11 -0700

    Update change in files login with format of text to json

--- Back/controllers/LoginController.js
+++ Back/controllers/LoginController.js
@@ -1,32 +1,32 @@
-import { validateUser } from './UsersController.js'
+import { validateUser } from "./UsersController.js";
 import { verified } from "../middlewares/encrypt.js";
 import jwt from "jsonwebtoken";
 import { TOKEN_KEY } from "../d_config.js";
 let role;
-let res_user= {};
+let res_user = {};
 
 export const login = async (req, res) => {
- 
   try {
     const { username, password } = req.body;
 
     if (!(username && password)) {
-      res.status(400).send("Indica username y contraseña");
-    }
-
-    const user = await validateUser(username);
-
-    if (user && (await verified(password, user.passHash))) {
-      const token = jwt.sign({ role }, TOKEN_KEY, { expiresIn: "2h" });
-      
-      res_user.role= user.role;
-      res_user.token = token;
-      
-      res.status(200).json(res_user);
+      res.status(400).send({"error":"Indica username y contraseña"});
     } else {
-      res.status(403).send("Credenciales inválidas");
+      const user = await validateUser(username);
+
+      if (user && (await verified(password, user.passHash))) {
+        const token = jwt.sign({ role }, TOKEN_KEY, { expiresIn: "2h" });
+
+        res_user.role = user.role;
+        res_user.token = token;
+
+        res.status(200).json(res_user);
+      } else {
+        res.status(403).json({"error":"Credenciales inválidas"});
+      }
     }
   } catch (err) {
     console.log("Ha ocurrido un error", err);
+    res.status(500).json(err);
   }
 };
