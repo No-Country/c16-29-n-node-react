50b64d523ce2d16fbd1cdef376ee1b5f5dc3d71d
emanuel-gauna <lemagauna@gmail.com>
2024-02-24 22:03:21 -0300

    moviendo el entrypoint a SRC

--- Back/src/app.js
+++ Back/src/app.js
@@ -1,41 +1,41 @@
-import {APP_PORT} from "../d_config.js"
-import express from "express"
-import cors from 'cors'
+import { PORT } from "../d_config.js";
+import  express  from "express";
+import  cors  from "cors";
 
-require("dotenv").config();
-const methodOverride = require('method-override');
+import db from './database/db.js'
 
-//import db from './database/db.js'
+import nonAttendancesRouter from './routes/NonAttendancesRoutes.js'
+import bannsRouter from './routes/BannsRoutes.js'
+import notesRouter from './routes/NotesRoutes.js'
+import LoginRouter  from "./routes/LoginRoutes.js"
+import UsersRouter  from "./routes/UsersRoutes.js"
 
+require( 'dotenv').config();
+const app = express();
 
-
-import persRouter from './routes/PersRoutes.js'
-
-const  [ markRouter, subjectsRouter ] = require("./routes");
-
-
-const app = express()
-
-app.use(cors())
+app.use(cors());
+app.use(express.json());
 app.use(express.urlencoded({ extended: false }));//captura eventos de formulario
-app.use(express.json())
-app.use(methodOverride('_method'));//habilitar metodos put y delete wn los form (?_method=PUT)(?_method=DELETE)
-app.use('/pers', persRouter)
-app.use('/login', persRouter)
+app.use(methodOverride('_method'));
+app.use("/api/nonattendances", nonAttendancesRouter);
+app.use("/api/banns", bannsRouter);
+app.use("/api/notes", notesRouter);
+app.use("/api/login", LoginRouter);
+app.use("/api/user", UsersRouter);
 app.use(`/api/marks`, markRouter);
 app.use(`/api/subjects`, subjectsRouter);
-/*try {
+
+try {
     await db.authenticate()
     console.log('Conexion exitosa a la DB')
 } catch (error) {
     console.log('Error de conexion a la DB =', error)
-}*/
-// Manejo de errores
+}
 app.use((err, req, res, next) => {
-    console.error(err);
-    res.status(500).json({ error: "Internal Server Error" });
+  console.error(err);
+  res.status(500).json({ error: "Internal Server Error" });
 });
 
-app.listen(APP_PORT, ()=>{
-    console.log(`Server UP run in https://localhost:${APP_PORT}/`)
-})
+app.listen(PORT, () => {
+  console.log(`Server UP run in https://localhost:${PORT}/`);
+});
