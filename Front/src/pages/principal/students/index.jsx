d1c0f65df4296588169068a702a26938a229dcd1
andrescn2020 <andrescn_2010@hotmail.com>
2024-02-26 11:22:40 -0300

    updating directive student view

--- Front/src/pages/principal/students/index.jsx
+++ Front/src/pages/principal/students/index.jsx
@@ -1,85 +1,180 @@
+// import { useMemo, useState } from "react";
+// import { SimpleTable } from "../../../components/SimpleTabla";
+// import MOCK from "./mock";
+// import { useRoutes, Link } from "react-router-dom";
+// import StudentView from "./see-student/index";
+// import Button from "../../../components/ui/button";
+
+// const PrincipalStudentsView = () => {
+//   const routes = useRoutes([
+//     { path: "/" , element: <StudentsView />},
+//     { path: "/:id", element: <StudentView />}
+//   ])
+
+//   return routes;
+// };
+
+// const StudentsView = () => {
+//   const handleAction = (action) => (row) => {
+//     console.log(action, row);
+//   }
+
+//   const [active, setActive] = useState(0);
+//   const [data, setData] = useState(MOCK);
+
+//   const filterSomething = (e) => {
+//     setData(data.filter((subject) => e.currentTarget.value))
+//   }
+
+//   const columns = useMemo(() => {
+//     return [
+//       {
+//         Header: "Nombre Completo",
+//         accessorKey: "name",
+//         cell: ({ row: { original } }) => <Link to={`${original.name} ${original.grade}° ${original.divition}`}>{original.name}</Link>
+//       },
+//       {
+//         id: "tutors",
+//         Header: "Tutores Asociados",
+//         accessorKey: "tutors"
+//       },
+//       {
+//         Header: "Estado",
+//         accessorKey: "state",
+//       },
+//       {
+//         Header: "Acciones",
+//         id: "actions",
+//         cell: ({ row: { original } }) => (
+//           <div className="flex jutify-center gap-2">
+//             <button onClick={() => handleAction("edit")(original)}>
+//               <img src="/assets/edit.svg" alt="editar materia" />
+//             </button>
+//             <button onClick={() => handleAction("delete")(original)}>
+//               <img src="/assets/trash.svg" alt="eliminar materia" />
+//             </button>
+//           </div>
+//         ),
+//       },
+//     ];
+//   }, [])
+
+//   return (
+//     <div className="grow flex flex-col overflow-hidden">
+//       <p className="mx-0">{data.length} registros</p>
+//       <SimpleTable columns={columns} data={data} actions={<Button>Crear Alumno</Button>} />
+//     </div>
+//   );
+// }
+
+// export default PrincipalStudentsView;
+
 import { useMemo, useState } from "react";
 import { SimpleTable } from "../../../components/SimpleTabla";
 import MOCK from "./mock";
-import { useRoutes, Link } from "react-router-dom";
-import StudentView from "./see-student/index";
+import { useRoutes } from "react-router-dom";
+import CreateSubject from "../../../components/forms/create-subject";
+import EditSubject from "../../../components/forms/edit-subject";
 import Button from "../../../components/ui/button";
+import Offcanvas from "../../../components/ui/offcanvas";
+import Modal from "../../../components/ui/modal";
+import useDisclosure from "../../../hooks/useDisclosure";
+import ConfirmDelete from "../../../components/modals/confirm-delete";
 
 const PrincipalStudentsView = () => {
   const routes = useRoutes([
-    { path: "/" , element: <StudentsView />},
-    { path: "/:id", element: <StudentView />}
+    { path: "/" , element: <StudentsView />}
   ])
 
   return routes;
 };
 
 const StudentsView = () => {
-  const handleAction = (action) => (row) => {
-    console.log(action, row);
-  }
-
-  const [active, setActive] = useState(0);
+  const [active, setActive] = useState({
+    type: "",
+    row: {}
+  });
+  const offcanvas = useDisclosure();
+  const modal = useDisclosure();
   const [data, setData] = useState(MOCK);
 
-  const filterSomething = (e) => {
-    setData(data.filter((subject) => e.currentTarget.value))
+  const resetState = (action) => () => {
+    action();
+    setActive({
+      type: "",
+      row: {}
+    });
+  } 
+
+  const handleConfirmCreateItem = () => {
+    setActive({
+      type: "create"
+    });
+    offcanvas.handleOpen();
   }
 
-  // const filters = [
-  //   (table) => (
-  //   <div className="border-2">
-  //     <label htmlFor="name"></label>
-  //     <input type="text" />
-  //   </div>
-  //   ),
-  //   (table) => (
-  //   <div className="border-2">
-  //     <label htmlFor="name"></label>
-  //     <input type="text" />
-  //   </div>
-  //   ),
-  // ]
+  const handleConfirmEditItem = (row) => {
+    setActive({
+      type: "edit",
+      row
+    })
+    offcanvas.handleOpen();
+  }
 
-  // const tabs = [
-  //   (table, index) => (
-  //     <div onClick={() => setActive(index)} className={`border-b-2 ${active === index ? "bg-slate-500" : ""}`}>
-  //       Materia
-  //     </div>
-  //     ),
-  //   (table, index) => (
-  //   <div onClick={() => setActive(index)} className={`border-b-2 ${active === index ? "bg-slate-500" : ""}`}>
-  //     Calificaciones
-  //   </div>
-  //   ),
-  //   (table, index) => (
-  //     <div onClick={() => setActive(index)} className={`border-b-2 ${active === index ? "bg-slate-500" : ""}`}>
-  //       Inasistencias
-  //     </div>
-  //     ),
-  //   (table, index) => (
-  //   <div onClick={() => setActive(index)} className={`border-b-2 ${active === index ? "bg-slate-500" : ""}`}>
-  //     Amonestaciones
-  //   </div>
-  //   ),
-  //   (table, index) => (
-  //     <div onClick={() => setActive(index)} className={`border-b-2 ${active === index ? "bg-slate-500" : ""}`}>
-  //       Anotaciones
-  //     </div>
-  //     ),
-  // ]
+  const handleEditItem = (row) => {
+    setData((data) => data.map((subject) => {
+      return subject.id === active.row.id 
+      ? (
+          {
+            ...row,
+            name: row.subject,
+            teachers: [{
+              id: row.teacher.value,
+              name: row.teacher.label,
+            }],
+            students: []
+          }
+        ) 
+      : (
+        subject
+      )
+    })
+    )
+  }
+
+  const handleCreateItem = (row) => {
+    setData((data) => data.concat({
+      ...row,
+      name: row.subject,
+      teachers: [{
+        id: row.teacher.value,
+        name: row.teacher.label,
+      }],
+      students: []
+    }))
+  }
+
+  const handleConfirmDeleteItem = (row) => {
+    setActive({
+      type: "delete",
+      row
+    })
+    modal.handleOpen();
+  }
+
+  const handleDeleteItem = ({ id }) => {
+    setData((subjects) => subjects.filter((subject) => subject.id !== id))
+  }
 
   const columns = useMemo(() => {
     return [
       {
         Header: "Nombre Completo",
         accessorKey: "name",
-        cell: ({ row: { original } }) => <Link to={`${original.name} ${original.grade}° ${original.divition}`}>{original.name}</Link>
       },
       {
-        id: "tutors",
-        Header: "Tutores Asociados",
-        accessorKey: "tutors"
+        Header: "Tutores asociados",
+        accessorKey: "tutors",
       },
       {
         Header: "Estado",
@@ -90,11 +185,11 @@ const StudentsView = () => {
         id: "actions",
         cell: ({ row: { original } }) => (
           <div className="flex jutify-center gap-2">
-            <button onClick={() => handleAction("edit")(original)}>
-              <img src="/assets/edit.svg" alt="editar materia" />
+            <button onClick={() => handleConfirmEditItem(original)}>
+              <img src="/assets/edit.svg" alt="editar alumno" />
             </button>
-            <button onClick={() => handleAction("delete")(original)}>
-              <img src="/assets/trash.svg" alt="eliminar materia" />
+            <button onClick={() => handleConfirmDeleteItem(original)}>
+              <img src="/assets/trash.svg" alt="eliminar alumno" />
             </button>
           </div>
         ),
@@ -104,8 +199,51 @@ const StudentsView = () => {
 
   return (
     <div className="grow flex flex-col overflow-auto">
-      <p>{data.length} registros</p>
-      <SimpleTable columns={columns} data={data} actions={<Button>Crear Alumno</Button>} />
+      <p>{data.length} Registros</p>
+      <SimpleTable 
+        columns={columns} 
+        data={data}
+        actions={<Button onClick={handleConfirmCreateItem}>Crear Alumno</Button>}
+      />
+      <Offcanvas
+        isOpen={offcanvas.isOpen}
+        onClose={resetState(offcanvas.handleClose)}
+        title={"Crear Materia"}
+      >
+        { active.type === "create" && (
+          <CreateSubject 
+            onClose={resetState(offcanvas.handleClose)}
+            onSubmit={handleCreateItem}
+          />
+        ) }
+        { active.type === "edit" && (
+          <EditSubject 
+            onClose={resetState(offcanvas.handleClose)}
+            onSubmit={handleEditItem}
+            initialValues={{
+              subject: active.row.name,
+              grade: active.row.grade.toString(),
+              divition: active.row.divition,
+              teacher: {
+                value: active.row.teachers[0].id,
+                label: active.row.teachers[0].name
+              }
+            }}
+          />
+        ) }
+      </Offcanvas>
+      <Modal
+        isOpen={modal.isOpen}
+        onClose={modal.handleClose}
+      >
+        { active.type === "delete" && (
+          <ConfirmDelete 
+            text={`${active.row.name} ${active.row.grade}° ${active.row.divition}`}
+            onClose={resetState(modal.handleClose)}
+            onConfirm={() => handleDeleteItem(active.row)}
+          />
+        ) }
+      </Modal>
     </div>
   );
 }
